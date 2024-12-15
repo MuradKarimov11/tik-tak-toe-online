13562eddb7297766f0c56fd2134679b45aa18e9c
Murad Karimov <muradkarimov07@gmail.com>
2024-12-15 13:42:15 +0300

    add base structure

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/shared/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button size="sm" variant={"destructive"}>
+        Hello Buy Ton coin
+      </Button>
+    </div>
+  );
+}
