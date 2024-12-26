621a538594f4be3887409fdc07ed87932f292db4
Murad Karimov <muradkarimov07@gmail.com>
2024-12-26 07:22:23 +0300

    add entities game-events and start-game files

--- src/entities/game/server.ts
+++ src/entities/game/server.ts
@@ -1,3 +1,4 @@
+export { startGame } from "./services/start-game";
 export { getGameById } from "./services/get-game";
 export { createGame } from "./services/create-game";
 export { getIdleGames } from "./services/get-idle-games";
