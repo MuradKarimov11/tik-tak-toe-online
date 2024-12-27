d9f3cfd5729115d088ffc8f2e9b8edea4e7c54da
Murad Karimov <muradkarimov07@gmail.com>
2024-12-27 11:15:24 +0300

    add entities step-game.ts and surrender-game.ts files

--- src/entities/game/server.ts
+++ src/entities/game/server.ts
@@ -1,4 +1,6 @@
+export { surrenderGame } from "./services/surrender-game";
 export { startGame } from "./services/start-game";
 export { getGameById } from "./services/get-game";
 export { createGame } from "./services/create-game";
 export { getIdleGames } from "./services/get-idle-games";
+export { stepGame } from "./services/step-game";
